7e82e06227cf7a9b125ee9b16c6ebf5a453e1d46
furkanyanmaz <60851738+furkanyanmaz@users.noreply.github.com>
2020-02-09 22:20:06 +0300

    Update filterPages.js

--- src/components/filter/filterPages.js
+++ src/components/filter/filterPages.js
@@ -1,33 +1,35 @@
 import React from "react";
 import {connect} from "react-redux";
+import {filterPrice} from "../../actions";
+
 
 class FilterPages extends React.Component{
     constructor(props) {
         super(props);
-
-        this.onChange   = this.onChange.bind(this);
+        this.onChange = this.onChange.bind(this);
 
         this.state = {
-            price:0
+            price:2000
         }
     }
-
 
     onChange(e){
         this.setState({
             [e.target.name]:e.target.value
         })
+
     }
 
     render(){
+        this.props.filterPrice({price:this.state.price})
         return(
             <div>
                 <form>
-                    <input type="number" name={"price"} value={this.state.price} onChange={this.onChange} />
+                    <input type="number" value={this.state.price} name={"price"} onChange={this.onChange} />
                 </form>
             </div>
         )
     }
 };
 
-export default connect(null)(FilterPages);
+export default connect(null,{filterPrice})(FilterPages);
